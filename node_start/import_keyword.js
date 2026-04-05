c3f977ecb99132489332a45f285a7e81164e9e2d
kuldeepjhorar40 <kuldeepjhorar40@gmail.com>
2026-04-05 13:42:03 +0530

    event bublling

--- node_start/import_keyword.js
+++ node_start/import_keyword.js
@@ -1,5 +1,6 @@
 // we can either use import or require in our code mainly one at a time
-
+// note:- import can give us power to import only required function from another file , but require select all function from file 
+// 3, import is more fast , memory mangement , asynhronous
 import {sub, mul} from "./export_for_import.js";
 
 console.log(sub(3,4));
